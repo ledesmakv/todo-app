7fe65305c2a70e5624e7ef0304bd0fd42c899e5a
Karina Ledesma <ledesmakv@gmail.com>
2021-11-02 23:10:09 -0300

    Setting up basic stuff

---
+++ src/components/AddTodoCard.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const AddTodoCard = () => {
+  return <Input />;
+};
+
+export default AddTodoCard;
+
+const Input = styled.div`
+  width: 280px;
+  height: 220px;
+  background-color: #fff;
+  border: 2px solid #000;
+  border-radius: 4px;
+`;
